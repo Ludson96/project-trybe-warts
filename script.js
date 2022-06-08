7e25d6c55513724066ce31de516e8b62ac982fc9
Ludson <ludson_ps27@hotmail.com>
2022-06-08 16:05:12 -0300

    requisito 21 incompleto

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Botão de login
 const getSub = document.getElementById('btn-submit');
 
 function valida() {
@@ -26,3 +27,36 @@ function btnValidation() {
 }
 
 agreement.addEventListener('click', btnValidation);
+
+const retName = document.getElementById('name-return');
+const retEmail = document.getElementById('email-return');
+const retHouse = document.getElementById('house-return');
+const retFamily = document.getElementById('family-return');
+const retMateria = document.getElementById('materia-return');
+const retAva = document.getElementById('ava-return');
+const retObs = document.getElementById('obs-return');
+// const logo = document.getElementById('trybewarts-forms-logo');
+const getNone = document.getElementById('container-none');
+const button = document.getElementById('button');
+const form = document.getElementById('form-data');
+const getName = document.getElementById('input-name');
+const getLast = document.getElementById('input-lastname');
+const getEmail = document.getElementById('input-email');
+const getgouse = document.getElementById('house');
+const getTextarea = document.getElementById('textarea');
+
+function returnValue(event) {
+  getNone.style.display = 'none';
+  event.preventDefault();
+  retName.innerText = `${getName.value} ${getLast.value}`;
+  retEmail.innerText = getEmail.value;
+  retHouse.innerText = getgouse.value;
+  // retFamily.innerText =
+  // retMateria.innerText =
+  // retAva.innerText =
+  retObs.innerText = getTextarea.value;
+  form.style.display = 'flex';
+  form.style.order = '0';
+}
+
+button.addEventListener('click', returnValue);
